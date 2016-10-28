7919fc1e6e2840a4f85a64e7049e039bd602e70f
Ka Wo Fong <kwfong@umass.edu>
2016-10-27 21:07:45 -0400

    initial test commit, answer question 1

---
+++ CoffeeMaker/README.txt
@@ -0,0 +1,39 @@
+1. What fault did you identify in the Inventory class? Make sure that you provide and reference a fault-triggering test.
+
+There are two faults in the Inventory class. The first fault is within the useIngredients() methods in Inventory class.
+After we call useIngredients(), the amount of coffee should be decremented. However, the source code contains a logical
+error and increments the amount of coffe instead.
+
+@Test
+public void testUseIngredients() {
+  Inventory inventory = new Inventory();
+  Recipe recipe = new Recipe();
+  try{
+    recipe.setAmtChocolate("2");
+    recipe.setAmtCoffee("2");
+    recipe.setAmtMilk("2");
+    recipe.setAmtSugar("2");
+  }catch(RecipeException e){
+    e.printStackTrace();
+  }
+  assertTrue(inventory.useIngredients(recipe));
+  assertEquals(13, inventory.getCoffee());
+  assertEquals(13, inventory.getSugar());
+  assertEquals(13, inventory.getMilk());
+  assertEquals(13, inventory.getChocolate());
+}
+
+The second fault is within addSugar() method in Inventory class. The addSugar method is only adding negative amount of sugar
+instead of positive amount. Hence, RecipeException will be thrown when a positive amount of sugar is passed into the method.
+
+2. Consider the faulty method that you identified, do the generated tests for this method pass or fail? Briefly explain why.
+
+3. What parts of the Inventory class are not covered by the generated tests? Briefly explain why these parts are challenging.
+
+4. How do the generated test suites differ from your manually written one, in particular w.r.t. the following:
+  generation time
+  number of test cases
+  average test case length
+  readability of test cases
+  correctness and usefulness
+  maintainability of test cases
--- CoffeeMaker/test/edu/ncsu/csc326/coffeemaker/InventoryTest.java
+++ CoffeeMaker/test/edu/ncsu/csc326/coffeemaker/InventoryTest.java
@@ -3,13 +3,130 @@ package edu.ncsu.csc326.coffeemaker;
 import org.junit.Test;
 import static org.junit.Assert.*;
 import edu.ncsu.csc326.coffeemaker.Inventory;
+import edu.ncsu.csc326.coffeemaker.exceptions.*;
 
 public class InventoryTest {
-	
+
 	@Test
-	public void testDefaults() {
+	public void testChocolate() {
 		Inventory inventory = new Inventory();
-		int chocUnits = inventory.getChocolate();
-		assertEquals(15, chocUnits);
+		int units = inventory.getChocolate();
+		assertEquals(15, units);
 	}
+
+	@Test
+	public void testAddChocolate() {
+		Inventory inventory = new Inventory();
+		try{
+			inventory.addChocolate("2");
+		}catch(InventoryException e){
+			e.printStackTrace();
+		}
+		int units = inventory.getChocolate();
+		assertEquals(17, units);
+	}
+
+	@Test
+	public void testMilk() {
+		Inventory inventory = new Inventory();
+		int units = inventory.getMilk();
+		assertEquals(15, units);
+	}
+
+	@Test
+	public void testAddMilk() {
+		Inventory inventory = new Inventory();
+		try{
+			inventory.addMilk("2");
+		}catch(InventoryException e){
+			e.printStackTrace();
+		}
+		int units = inventory.getMilk();
+		assertEquals(17, units);
+	}
+
+	@Test
+	public void testSugar() {
+		Inventory inventory = new Inventory();
+		int units = inventory.getSugar();
+		assertEquals(15, units);
+	}
+
+	@Test
+	public void testAddSugar() {
+		Inventory inventory = new Inventory();
+		try{
+			inventory.addSugar("2");
+		}catch(InventoryException e){
+			// e.printStackTrace();
+			fail("InventoryException should not occur when adding positive sugar!");
+		}
+		int units = inventory.getSugar();
+		assertEquals(17, units);
+	}
+
+	@Test(expected=InventoryException.class)
+	public void testAddNegativeSugar(){
+		Inventory inventory = new Inventory();
+		try{
+			inventory.addSugar("-2");
+		}catch(InventoryException e){
+			e.printStackTrace();
+		}
+	}
+
+	@Test
+	public void testCoffee() {
+		Inventory inventory = new Inventory();
+		int units = inventory.getCoffee();
+		assertEquals(15, units);
+	}
+
+	@Test
+	public void testAddCoffee() {
+		Inventory inventory = new Inventory();
+		try{
+			inventory.addCoffee("2");
+		}catch(InventoryException e){
+			e.printStackTrace();
+		}
+		int units = inventory.getCoffee();
+		assertEquals(17, units);
+	}
+
+	@Test
+	public void testEnoughIngredients() {
+		Inventory inventory = new Inventory();
+		Recipe recipe = new Recipe();
+		try{
+			recipe.setAmtChocolate("2");
+			recipe.setAmtCoffee("2");
+			recipe.setAmtMilk("2");
+			recipe.setAmtSugar("2");
+		}catch(RecipeException e){
+			e.printStackTrace();
+		}
+		assertEquals(true, inventory.enoughIngredients(recipe));
+	}
+
+	@Test
+	public void testUseIngredients() {
+		Inventory inventory = new Inventory();
+		Recipe recipe = new Recipe();
+		try{
+			recipe.setAmtChocolate("2");
+			recipe.setAmtCoffee("2");
+			recipe.setAmtMilk("2");
+			recipe.setAmtSugar("2");
+		}catch(RecipeException e){
+			e.printStackTrace();
+		}
+
+		assertTrue(inventory.useIngredients(recipe));
+		assertEquals(13, inventory.getCoffee());
+		assertEquals(13, inventory.getSugar());
+		assertEquals(13, inventory.getMilk());
+		assertEquals(13, inventory.getChocolate());
+	}
+
 }
